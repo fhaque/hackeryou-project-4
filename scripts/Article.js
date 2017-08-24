0092bd1ec7926707d5629e3f33ec02baf5cae741
fhaque <frhaque@gmail.com>
2017-08-24 15:37:22 -0400

    first commit

---
+++ scripts/Article.js
@@ -0,0 +1,90 @@
+/*
+REQUIRES:
+commonWords.js
+Tiny JavaScript tokenizer.js
+PorterStemmer2.min.js
+levenshtein.min.js
+*/
+class Article {
+    constructor(articleEntry) {
+        this.articleEntry = articleEntry;
+        
+        this.summaryObj = Article.constructWordObjOf(articleEntry.summary);
+        this.titleObj = Article.constructWordObjOf(articleEntry.title);
+    }
+
+    static constructWordObjOf(string) {
+        let words = {};
+
+        string = string.toLowerCase();
+
+        let tokenizedWords = Article.tokenize(string);
+
+        tokenizedWords = Article.filterTokenizedWordsFor('word', tokenizedWords);
+
+        tokenizedWords = Article.stemTokenizedWords(tokenizedWords);
+
+        words = Article.tokenizedWordsToWordObj(tokenizedWords);
+
+        words = Article.removeCommonWordsFromWordsObj(words);
+
+        words = Article.removeWordsWithWordCountLessThan(2, words);
+
+        return words;
+    }
+
+    static tokenize(string) {
+        return tokenize(string, { word:/\w+/, whitespace:/\s+/, punctuation:/[^\w\s]/}, 'invalid');
+    }
+
+    static filterTokenizedWordsFor(type, tokenizedWords) {
+        return tokenizedWords.filter( (obj) => obj.type === type );
+    }
+
+    static stemTokenizedWords(tokenizedWords) {
+        return tokenizedWords.map( (obj) => stemmer(obj.token) );
+    }
+
+    static removeCommonWordsFromWordsObj(wordsObj) {
+        commonWords.forEach( (word) => {
+            if (word in wordsObj) {
+                delete wordsObj[word];
+            }
+        });
+
+        return wordsObj;
+    }
+
+    static removeWordsWithWordCountLessThan(count, wordsObj) {
+        for(let word in wordsObj) {
+            if (wordsObj[word] < count) {
+                delete wordsObj[word];
+            }
+        }
+
+        return wordsObj;
+    }
+
+    static totalWordCountOfWordsObj(wordsObj) {
+        let sum = 0;
+        for ( let word in wordsObj ) {
+            sum += a[word];
+        }
+    
+        return sum;
+    }
+
+    static tokenizedWordsToWordObj(tokenizedWords) {
+        const words = {};
+
+        for (let word of tokenizedWords) {
+            if ( !(word in words) ) {
+                words[word] = 1;
+            } else {
+                words[word]++;
+            }
+        }
+
+        return words;
+    }
+}
---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+// const article1 = {};
+
+// article1.summary = `We propose a strategy to measure weak static magnetic fields with nitrogen-vacancy color center in diamond. Inspired by avian magnetoreception models, we consider the feasibility of utilizing quantum coherence phenomena to measure weak static magnetic fields. Nitrogen-vacancy (NV) color centers are regarded as the ideal platform to study quantum sciences as a result of its long coherence time up to a millisecond timescale. In high-purity diamond, hyperfine interaction with 13C nuclear spins dominates the decoherence process. In this paper, we numerically simulate the decoherence process between 0 and +1 of the individual NV color center spin in 13C nuclear baths with various of magnitudes of external magnetic fields. By applying Hahn echo into the system, we obtain the coherence of NV color center spin as a function of total evolution time and magnetic field. Furthermore we obtain the high-accuracy relationship between the three decoherence-characteristic timescales, i.e. T_W, T_R, T_2, and magnetic field B. And we draw a conclusion that T_R has the highest sensitivity about magnetic field among the three time-scales. Thus, for a certain NV color center, T_R can be the scale for the magnitude of magnetic field, or rather, the component along the NV electronic spin axis. When measuring an unknown magnetic field, we adjust the NV axis to three mutually orthogonal directions respectively. By this means, we obtain the three components of the magnetic field and thus the magnitude and direction of the actual magnetic field. The accuracy could reach 60 nT/Hz^{1/2},and could be greatly improved by using an ensemble of NV color centers or diamond crystals purified with 12C atoms.`;
+
+// article1.title = `Proton-ring and Electron-linac Collider (PRELC) as a (first)
+// TeV-rangeelectron-proton or photon-proton collider`
+
+const xmlConverter = new X2JS();
+const listOfArticles = [];
+
+	$.ajax({
+	url: 'http://export.arxiv.org/api/query?search_query=nitrogen+AND+vacancy',
+	dataType: 'xml',
+	method:'GET',
+	// data: {
+	// 	'search_query': "all:electron+AND+all:proton",
+	// },
+    }).then(function(res) {
+		console.log(res);
+        
+        xmlConverter.xml2json(res).feed.entry.forEach( (article) => {
+            listOfArticles.push( new Article(article) );
+        });
+
+        console.log(listOfArticles);
+        
+		// console.log(xmlConverter.xml2json(res).feed.entry);
+	});
+
+// console.log(new Article(article1));
